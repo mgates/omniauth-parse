564f0be86234ce0cadf80b74cd92d35ea8a5abac
Micah Gates <github@mgates.com>
2012-07-02 13:41:34 -0500

    moving stuff around

--- lib/omniauth-parse-com.rb
+++
@@ -1,9 +0,0 @@
-require "omniauth-parse-com/version"
-
-module Omniauth
-  module Parse
-    module Com
-      # Your code goes here...
-    end
-  end
-end
--- lib/omniauth-parse-com/version.rb
+++
@@ -1,7 +0,0 @@
-module Omniauth
-  module Parse
-    module Com
-      VERSION = "0.0.1"
-    end
-  end
-end
