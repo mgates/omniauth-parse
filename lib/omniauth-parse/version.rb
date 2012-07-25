e8601f750e1de753dab17c1c70472ca3f2303fd8
Micah Gates <github@mgates.com>
2012-07-25 15:08:00 -0500

    password field for password

--- lib/omniauth-parse/version.rb
+++ lib/omniauth-parse/version.rb
@@ -1,5 +1,5 @@
 module OmniAuth
   module Parse
-    VERSION = '0.0.7'
+    VERSION = '0.0.9'
   end
 end
--- lib/omniauth/strategies/parse.rb
+++ lib/omniauth/strategies/parse.rb
@@ -8,49 +8,49 @@ module OmniAuth
 
       PARSE_LOGIN = {:site => "https://api.parse.com", :path => "1/login"}
 
-      option :fields, [:name, :username]
+      option :fields, [:username, :password]
       option :uid_field, :username
 
-      def initialize(configuration)
-        @application_id = configuration[:application_id] or raise "application_id is required for initialization"
-        @rest_api_key = configuration[:rest_api_key] or raise "rest_api_key is required for initialization"
+      def initialize(app, options = {})
+        @application_id = options[:application_id] or raise "application_id is required for initialization"
+        @rest_api_key = options[:rest_api_key] or raise "rest_api_key is required for initialization"
+        super
       end
 
       def request_phase
         form = OmniAuth::Form.new(:title => "Parse.com Info", :url => callback_path)
-          options.fields.each do |field|
-          form.text_field field.to_s.capitalize.gsub("_", " "), field.to_s
-        end
+          form.text_field :username, :username
+          form.password_field :password, :password
         form.button "Sign In"
         form.to_response
       end
 
       def callback_phase
-        return fail!(:invalid_credentials) unless is_valid_user
+        @raw_data = is_valid_user
+        return fail!(:invalid_credentials) unless @raw_data
+        debugger
         super
       end
 
       def is_valid_user
         conn = Faraday.new(:url => PARSE_LOGIN[:site]) do |req|
           req.request :url_encoded
-          faraday.adapter  Faraday.default_adapter
+          req.adapter  Faraday.default_adapter
         end
 
         result = conn.get do |req|
           req.url PARSE_LOGIN[:path]
           req.headers['X-Parse-Application-Id'] = @application_id
           req.headers['X-Parse-REST-API-Key'] = @rest_api_key
-          req.params['username'] = "XXX"
-          req.params['password'] = "XXX"
+          req.params['username'] = request.params["username"]
+          req.params['password'] = request.params["password"]
         end
-
-        unless result.status == "200"
+        unless result.status == 200
           #logging?
           return false
         end
-
+        return result.body
       end
-
     end
   end
 end
