262d18e435277859eca914e9591469aa013051f9
Micah Gates <github@mgates.com>
2012-07-27 11:35:27 -0500

    I think that was a c&p bogon

--- spec/omniauth/strategies/parse.rb
+++ spec/omniauth/strategies/parse.rb
@@ -3,10 +3,10 @@ require File.dirname(__FILE__) + '/../../spec_helper'
 describe OmniAuth::Strategies::Parse, :type=> :strategy do
   include OmniAuth::Test::StrategyTestCase
   def strategy
-    @crowd_server_url ||= 'https://api.parse.com'
+    @server_url ||= 'https://api.parse.com'
     @application_name ||= 'bogus_app'
     @application_password ||= 'bogus_app_password'
-    [OmniAuth::Strategies::Parse, {:crowd_server_url => @crowd_server_url, :application_name=>@application_name, :application_password=>@application_password}]
+    [OmniAuth::Strategies::Parse, {:server_url => @crowd_server_url, :application_name=>@application_name, :application_password=>@application_password}]
   end
 
   describe "somthing" do
