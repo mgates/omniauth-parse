50dac6293c9cf5e64c138b9a525a82ac7225cb93
Micah Gates <github@mgates.com>
2012-07-30 09:54:30 -0500

    get rake spec working

--- Rakefile
+++ Rakefile
@@ -1,2 +1,10 @@
 #!/usr/bin/env rake
+require "bundler"
 require "bundler/gem_tasks"
+require 'rspec/core/rake_task'
+
+desc "Run specs"
+RSpec::Core::RakeTask.new do |t|
+  t.pattern = "./spec/**/*_spec.rb" # don't need this, it's default.
+end
+
---
+++ spec/omniauth/strategies/parse_spec.rb
@@ -0,0 +1,15 @@
+require File.dirname(__FILE__) + '/../../spec_helper'
+
+describe OmniAuth::Strategies::Parse, :type=> :strategy do
+  include OmniAuth::Test::StrategyTestCase
+  def strategy
+    @server_url ||= 'https://api.parse.com'
+    @application_name ||= 'bogus_app'
+    @application_password ||= 'bogus_app_password'
+    [OmniAuth::Strategies::Parse, {:server_url => @crowd_server_url, :application_name=>@application_name, :application_password=>@application_password}]
+  end
+
+end
+
+
+
